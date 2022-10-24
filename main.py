f30ca322e091cfd1ef67a2bda011824622bdeddc
Linh <vtn.linh266@gmail.com>
2022-10-24 23:18:38 +0700

    Fix Error Condition

--- main.py
+++ main.py
@@ -10,105 +10,146 @@ Checking the version of chatterbot compatibility with Python that you have insta
 '''Errors 3: ImportError: cannot import name 'ChatBot'
 Change chatterbot.trainers to chatterbot
 '''
-'''Error 4: AttributeError: 'str' object has no attribute 'get' (Python)
-Wrong YAML format
+'''AttributeError: '_tkinter.tkapp' object has no attribute 'Message' althought import * of tkinter
 '''
 '''Steps:
-1. Install chatterbot (command: pip install chatterbot==1.0.2)
-2. Install pyttsx3 (command: pip uninstall pyttsx3 and pip install pyttsx3 and pip install --upgrade comtypes)
-3. Install ssl <error installing nltk supporting packages>
-4. Install nltk <error installing nltk supporting packages>
+Step 1 Prepare the Dependencies
++Install chatterbot (command: pip install chatterbot==1.0.2)
++Install pyttsx3 (command: pip uninstall pyttsx3 and pip install pyttsx3 and pip install --upgrade comtypes)
++Install ssl <error installing nltk supporting packages>
++Install nltk <error installing nltk supporting packages>
+
+Step 2 Import Classes
+
+Step 3 Create and Train the Chatbot
+
+Step 4 Create Function (Get input - process input - return reponse)
+
+Step 5 GUI
 '''
 # Importing the modules – tkinter
 # are needed
 from tkinter import *
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from chatterbot import response_selection
 import pyttsx3 # to speech conversion
-import ssl
-import nltk
+from nltk.corpus import *
+import re
+import os
 
-# Conversations are stored as a dictionary 
-'''dialogue = [
+# Building a dictionary of responses
+dialogue = [
     'hello',
-    'hi there',
-    'what is your name?',
-    'My name is BOT, I am created my a hooman ',
+        'hi there!',
+    'what is your name',
+        'My name is BOT, I am created by AI',
     'how are you',
-    'i am good! Things are going pretty well for me',
+        'i am good!',
     'in which city you live?',
-    'i live in Ho Chi Minh City',
+        'i live in Ho Chi Minh City',
     'in which languages do you speak?',
-    'i mostly talk in english',
-    'igotta go',
-    'bye',
+        'i mostly talk in english',
+    'i gotta go',
+        'bye, nice to talk to you',
     'thanks',
-    'thank you'
+        'You are welcome']
 
-]'''
+sport_talk = [  
+    'WHAT IS BASKETBALL',
+        'A game with tall players.',
+    'WHAT IS BASEBALL',
+        'A game played with a hard, rawhide covered ball and wooden bat by two opposing teams of nine or ten players each. It is played on a field with four bases forming a diamond-shaped circuit.',
+    'WHAT IS SOCCER?',
+        'A game played with a round ball by two teams of eleven players on a field with a goal at either end; the ball is moved chiefly by kicking or by using any part of the body except the hands and arms.']
 
+health_talk = [
+    'how is your health',
+        'I am not feeling well',
+    'why?',
+        'I have a fever',
+    'did you take medicine?',
+       'Yes.',
+    'when',
+        'In the morning',
+        'Get well soon dear']
 
 '''
 Setting the training class via list data
 
 '''
-'''
-Way 1: ListTrainer class allows you to train from a List Data Structure while
-'''
-#bot = ChatBot("My Bot")
-#trainer = ListTrainer(bot) 
-#trainer.train(dialogue)
+bot = ChatBot(name='MyBOT', read_only=True,
+                logic_adapters= ['chatterbot.logic.MathematicalEvaluation',
+                                    'chatterbot.logic.BestMatch'])
+trainer = ListTrainer(bot)
+for item in (dialogue, sport_talk, health_talk):
+    trainer.train(item)
 
-'''
-Way 2: ChatterBotCorpusTrainer allows you to train from YAML or JSON data.
-'''
-# Create a new instance of a ChatBot
-bot = ChatBot(name='MyBOT',read_only = True,
-                 response_selection_method=response_selection.get_random_response,
-                 logic_adapters=[
-        {
-            'import_path': 'chatterbot.logic.SpecificResponseAdapter',
-            'input_text': 'empty',
-            'output_text': ''
-        },
-        {   
-            'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'sorry, I have no answer',
-            'maximum_similarity_threshold': 0.9
-        },
-        {
-            'import_path': 'chatterbot.logic.MathematicalEvaluation'
-        }
+print ("Welcome to MyBOT. Starting Q&A in few seconds!")
 
-    ]
-    )
-trainer = ChatterBotCorpusTrainer(bot)
-
-trainer.train('conversations')
-
-# Define a function to close the window
+# Define function
 def close():
-   #root.destroy()
-   root.quit()
+    #root.quit() --> the mainloop will still be running
+    #to close the window
+    root.after(60, root.destroy)
 
-# Chat function
+def botSpeak(voice):
+    # to speak
+    pyttsx3.speak(voice)
+
 def botReply():
-    print ("Welcome to MyBOT. How may I help you?")
-
+    global question
+    # Takes the user input and converts all characters to capitalize
     question = questionField.get()
+    # Initializing res - end number
+    regSign = re.search(r'\d+$', question) 
+    # A list of special_characters to be removed
+    special_characters = ['@','#','$','*','&', '!']
+
+    # Defining the Chatbot's condition
+    if question == 'quit':
+        print ("User is about to leave.")
+        botSpeak('Bye! Take care...')
+        close()
+    elif len(question) == 0:
+        print ("No question.")
+        botSpeak('No worries. Let me know when you have.')
+        close()
+    elif (regSign is not None) or question.endswith(tuple(special_characters)) or (bool(re.search('^[a-zA-Z0-9]*$',question))==True) :
+        # if the string ends in digits regSign will be a Match object, or None otherwise.
+        #print (regSign.group())
+        print ("Invalid question is", question)
+        botSpeak("Sorry, I didn't understand your question")
+        return question
+    elif question not in dialogue or question not in sport_talk or question not in health_talk:
+        print ("Question not found in list data")
+        botSpeak("It's not easy. Ask another question, please!")
+        return question
+    else:
+        print ("User has raised question.")
+        # The chatbot prints the response that matches the selected dialogue
+        if(question =='thanks' or question=='thank you' or question == 'bye'):
+            print("Bot: Thank you for visting..")
+            botSpeak("You're welcome. See you!")
+            close()
+        # To be continued
+        # elif(question.endswith(tuple(special_characters))):
+        #     for i in special_characters:
+                
+        #         # Replace the special character with ?
+        #         question=question.replace(i,"?")
+        #         print(question,"after replaching with question mark")
+        # elif((bool(re.search('^[a-zA-Z0-9]*$',question))==True)):
+        #     # Remove Special Chars
+        #     new_question = question
+        #     new_question = re.sub('[^a-zA-Z0-9 \n\.]', '', new_question)
+        #     print(question, "after removal of special characters")
+
+        print ("Let Bot think..")
     question = question.capitalize()
-    '''if question =='exit' or question =='Null':
-        print ("Thank you for visiting.")
-        # Create a Button to call close()
-        Button(root, text= "Close the Window", font=("Calibri",14,"bold"), command=close).pack(pady=20)
-        break'''
     answer = bot.get_response(question)
-    #print(answer)
     textarea.insert(END,'You: '+question+'\n\n')
     textarea.insert(END,'Bot: '+str(answer)+'\n\n')
-    #for Bot to speak
+    # for Bot to speak
     pyttsx3.speak(answer)
     questionField.delete(0,END)
 
@@ -117,16 +158,14 @@ def botReply():
 Widgets are added here
 '''
 root = Tk()
-root.title('ChatBot')
+root.title('MyBOT')
 
 # Set the size and the background of the window
 root.geometry('500x570+100+30')
 root.config(bg='deep pink')
-root.resizable(width=FALSE, height=FALSE)
 
-# Load the image
+# Load the image and display the image
 logoPic = PhotoImage(file='logo.png')
-# Add a label widget to display the image
 logoPicLabel = Label(root,image=logoPic,bg='deep pink')
 logoPicLabel.pack(pady=5)
 
@@ -136,25 +175,24 @@ centerFrame.pack()
 scrollbar = Scrollbar(centerFrame)
 scrollbar.pack(side=RIGHT)
 
-textarea = Text(centerFrame,font=('Helvetica 13',20,'bold'),height=10,yscrollcommand=scrollbar.set,wrap='word')
+textarea = Text(centerFrame,fg='deep pink',font=('Helvetica 13',20,'bold'),height=10,yscrollcommand=scrollbar.set,wrap='word')
 textarea.pack(side=LEFT)
 scrollbar.config(command=textarea.yview)
 
 questionField = Entry(root,font=('Helvetica 13',20,'bold'))
 questionField.pack(pady=15,fill=X)
 
-askPic = PhotoImage(file='ask.png')
+# Display a welcome message
 
-# Create Button and add function
+# Create Button and add botReply function
+askPic = PhotoImage(file='ask.png')
 askButton = Button(root,image=askPic,command=botReply)
 askButton.pack()
 
 def click(event):
     askButton.invoke()
 
-# bind the event "click" of the
-# root to the socket
-textarea.tag_configure('answer',foreground='green')
+# bind the event "click" of the root
 root.bind('<Return>',click)
 
 # Execute Tkinter
